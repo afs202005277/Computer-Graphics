e39b50577699281630c75374556da6ec5603bf48
Pedro Fonseca <pedrojgfonseca@gmail.com>
2023-02-14 09:50:36 +0000

    Parallelogram

--- tp1/MyScene.js
+++ tp1/MyScene.js
@@ -1,6 +1,7 @@
 import { CGFscene, CGFcamera, CGFaxis } from "../lib/CGF.js";
 import { MyDiamond } from "./MyDiamond.js";
 import { MyTriangle } from './MyTriangle.js';
+import { MyParallelogram } from './MyParallelogram.js'
 
 /**
  * MyScene
@@ -26,7 +27,9 @@ export class MyScene extends CGFscene {
 
     //Initialize scene objects
     this.axis = new CGFaxis(this);
+    this.diamond = new MyDiamond(this);
     this.triangle = new MyTriangle(this);
+    this.parallelogram = new MyParallelogram(this);
 
     //Objects connected to MyInterface
     this.displayAxis = true;
@@ -92,7 +95,10 @@ export class MyScene extends CGFscene {
 
     // ---- BEGIN Primitive drawing section
 
-    this.triangle.display();
+    //this.diamond.display();
+    //this.triangle.display();
+    this.parallelogram.display();
+    
 
     // ---- END Primitive drawing section
   }
