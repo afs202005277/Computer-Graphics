71b4456b6b5d756523b3e714d34b29e6847fcbe1
Andre Sousa <up202005277@up.pt>
2023-03-14 10:02:02 +0000

    did tangram

--- tp4/MyTangram.js
+++ tp4/MyTangram.js
@@ -1,226 +1,280 @@
 import {CGFappearance, CGFobject, CGFtexture} from '../lib/CGF.js';
-import { MyDiamond } from "./MyDiamond.js";
-import { MyTriangle } from './MyTriangle.js';
-import { MyParallelogram } from './MyParallelogram.js'
-import { MyTriangleSmall } from './MyTriangleSmall.js'
-import { MyTriangleBig } from "./MyTriangleBig.js";
+import {MyDiamond} from "./MyDiamond.js";
+import {MyTriangle} from './MyTriangle.js';
+import {MyParallelogram} from './MyParallelogram.js'
+import {MyTriangleSmall} from './MyTriangleSmall.js'
+import {MyTriangleBig} from "./MyTriangleBig.js";
+
 /**
  * MyDiamond
  * @constructor
  * @param scene - Reference to MyScene object
  */
 export class MyTangram extends CGFobject {
-	constructor(scene) {
-		super(scene);
+    constructor(scene) {
+        super(scene);
 
-		this.diamond = new MyDiamond(scene);
-		this.triangle = new MyTriangle(scene);
-		this.parallelogram = new MyParallelogram(scene);
-		this.trianglesmall1 = new MyTriangleSmall(scene);
-		this.trianglesmall2 = new MyTriangleSmall(scene);
-		this.bigTriangle1 = new MyTriangleBig(scene);
-		this.bigTriangle2 = new MyTriangleBig(scene);
+        this.texture = new CGFtexture(scene, 'images/tangram.png');
 
-		this.Mdiamond = new CGFappearance(scene);
-		this.Mdiamond.setAmbient(0.46,0.98, 0.30, 1.0);
-		this.Mdiamond.setDiffuse(0.46,0.98, 0.30, 1.0);
-		this.Mdiamond.setSpecular(0,1, 0, 1.0);
-		this.Mdiamond.setShininess(32.0);
-		this.Mdiamond.loadTexture('images/tangram.png');
-		this.Mdiamond.setTextureWrap('REPEAT', 'REPEAT');
 
-		this.MTriangleBlue = new CGFappearance(scene);
-		this.MTriangleBlue.setAmbient(0.25,0.61, 0.97, 1.0);
-		this.MTriangleBlue.setDiffuse(0.25,0.61, 0.97, 1.0);
-		this.MTriangleBlue.setSpecular(0,0, 1, 1.0);
-		this.MTriangleBlue.setShininess(32.0);
+        this.diamond = new MyDiamond(scene);
+        this.triangle = new MyTriangle(scene);
+        this.parallelogram = new MyParallelogram(scene);
+        this.trianglesmall1 = new MyTriangleSmall(scene);
+        this.trianglesmall2 = new MyTriangleSmall(scene);
+        this.bigTriangle1 = new MyTriangleBig(scene);
+        this.bigTriangle2 = new MyTriangleBig(scene);
 
-		this.orange = new CGFappearance(scene);
-		this.orange.setAmbient(0.95, 0.62, 0.22, 1.0);
-		this.orange.setDiffuse(0.95, 0.62, 0.22, 1.0);
-		this.orange.setSpecular(1,0.5, 0, 1.0);
-		this.orange.setShininess(32.0);
+        this.Mdiamond = new CGFappearance(scene);
+        this.Mdiamond.setAmbient(1, 1, 1, 1.0);
+        this.Mdiamond.setDiffuse(1, 1, 1, 1.0);
+        this.Mdiamond.setSpecular(1, 1, 1, 1.0);
+        this.Mdiamond.setShininess(32.0);
+        this.Mdiamond.setTexture(this.texture);
+        this.Mdiamond.setTextureWrap('REPEAT', 'REPEAT');
 
-		this.red = new CGFappearance(scene);
-		this.red.setAmbient(0.92, 0.23, 0.18, 1.0);
-		this.red.setDiffuse(0.92, 0.23, 0.18, 1.0);
-		this.red.setSpecular(1,0, 0, 1.0);
-		this.red.setShininess(32.0);
+        this.MTriangleBlue = new CGFappearance(scene);
+        this.MTriangleBlue.setAmbient(1, 1, 1, 1.0);
+        this.MTriangleBlue.setDiffuse(1, 1, 1, 1.0);
+        this.MTriangleBlue.setSpecular(1, 1, 1, 1.0);
+        this.MTriangleBlue.setShininess(32.0);
+        this.MTriangleBlue.setTexture(this.texture);
+        this.MTriangleBlue.setTextureWrap('REPEAT', 'REPEAT');
 
-		this.purple = new CGFappearance(scene);
-		this.purple.setAmbient(0.55, 0.34, 0.72, 1.0);
-		this.purple.setDiffuse(0.55, 0.34, 0.72, 1.0);
-		this.purple.setSpecular(0.5,0, 1, 1.0);
-		this.purple.setShininess(32.0);
+        this.orange = new CGFappearance(scene);
+        this.orange.setAmbient(1, 1, 1, 1.0);
+        this.orange.setDiffuse(1, 1, 1, 1.0);
+        this.orange.setSpecular(1, 1, 1, 1.0);
+        this.orange.setShininess(32.0);
+        this.orange.setTexture(this.texture);
+        this.orange.setTextureWrap('REPEAT', 'REPEAT');
 
-		this.pink = new CGFappearance(scene);
-		this.pink.setAmbient(0.95, 0.63, 0.80, 1.0);
-		this.pink.setDiffuse(0.95, 0.63, 0.80, 1.0);
-		this.pink.setSpecular(1,0.5, 1, 1.0);
-		this.pink.setShininess(32.0);
+        this.red = new CGFappearance(scene);
+        this.red.setAmbient(1, 1, 1, 1.0);
+        this.red.setDiffuse(1, 1, 1, 1.0);
+        this.red.setSpecular(1, 1, 1, 1.0);
+        this.red.setShininess(32.0);
+        this.red.setTexture(this.texture);
+        this.red.setTextureWrap('REPEAT', 'REPEAT');
 
-		this.yellow = new CGFappearance(scene);
-		this.yellow.setAmbient(1, 0.99, 0.33, 1.0);
-		this.yellow.setDiffuse(1, 0.99, 0.33, 1.0);
-		this.yellow.setSpecular(1,1, 0, 1.0);
-		this.yellow.setShininess(32.0);
-	}
+        this.purple = new CGFappearance(scene);
+        this.purple.setAmbient(1, 1, 1, 1.0);
+        this.purple.setDiffuse(1, 1, 1, 1.0);
+        this.purple.setSpecular(1, 1, 1, 1.0);
+        this.purple.setShininess(32.0);
+        this.purple.setTexture(this.texture);
+        this.purple.setTextureWrap('REPEAT', 'REPEAT');
 
-	display() {
+        this.pink = new CGFappearance(scene);
+        this.pink.setAmbient(1, 1, 1, 1.0);
+        this.pink.setDiffuse(1, 1, 1, 1.0);
+        this.pink.setSpecular(1, 1, 1, 1.0);
+        this.pink.setShininess(32.0);
+        this.pink.setTexture(this.texture);
+        this.pink.setTextureWrap('REPEAT', 'REPEAT');
 
-		this.scene.pushMatrix();
+        this.yellow = new CGFappearance(scene);
+        this.yellow.setAmbient(1, 1, 1, 1.0);
+        this.yellow.setDiffuse(1, 1, 1, 1.0);
+        this.yellow.setSpecular(1, 1, 1, 1.0);
+        this.yellow.setShininess(32.0);
+        this.yellow.setTexture(this.texture);
+        this.yellow.setTextureWrap('REPEAT', 'REPEAT');
+    }
 
-		var tra = [
-			1.0, 0.0, 0.0, 0.0,
-			0.0, 1.0, 0.0, 0.0,
-			0.0, 0.0, 1.0, 0.0,
-			0.4, 3.4, 0.0, 1.0
-		];
+    display() {
+        this.Mdiamond.apply();
 
-		this.scene.multMatrix(tra);
+        this.scene.pushMatrix();
 
-		this.Mdiamond.apply();
-		this.diamond.display();
+        var tra = [
+            1.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.4, 3.4, 0.0, 1.0
+        ];
 
-		this.scene.popMatrix();
+        this.scene.multMatrix(tra);
 
-		this.scene.pushMatrix();
+        this.diamond.display();
 
-		tra = [
-			1.0, 0.0, 0.0, 0.0,
-			0.0, 1.0, 0.0, 0.0,
-			0.0, 0.0, 1.0, 0.0,
-			0.0, 0.8, 0.0, 1.0
-		];
+        this.scene.popMatrix();
 
-		this.scene.multMatrix(tra);
+        this.scene.pushMatrix();
 
-		this.MTriangleBlue.apply();
-		this.bigTriangle1.display();
+        tra = [
+            1.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.0, 0.8, 0.0, 1.0
+        ];
 
-		this.scene.popMatrix();
+        this.scene.multMatrix(tra);
 
-		this.scene.pushMatrix();
+        this.bigTriangle1.texCoords = [
+            0, 0,
+            1, 0,
+            0.5, 0.5
+        ];
+        this.bigTriangle1.updateTexCoordsGLBuffers();
+        this.MTriangleBlue.apply();
+        this.bigTriangle1.display();
 
-		var tra1 = [
-			1.0, 0.0, 0.0, 0.0,
-			0.0, 1.0, 0.0, 0.0,
-			0.0, 0.0, 1.0, 0.0,
-			-2.0, 0.0, 0.0, 1.0
-		];
+        this.scene.popMatrix();
 
-		var rot = [
-			Math.cos(Math.PI / 4), Math.sin(Math.PI / 4), 0.0, 0.0,
-			-Math.sin(Math.PI / 4), Math.cos(Math.PI / 4), 0.0, 0.0,
-			0.0, 0.0, 1.0, 0.0,
-			0.0, 0.8, 0.0, 1.0
-		];
+        this.scene.pushMatrix();
 
-		var tra2 = [
-			1.0, 0.0, 0.0, 0.0,
-			0.0, 1.0, 0.0, 0.0,
-			0.0, 0.0, 1.0, 0.0,
-			0.0, -2.0, 0.0, 1.0
-		];
+        var tra1 = [
+            1.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            -2.0, 0.0, 0.0, 1.0
+        ];
 
-		this.scene.multMatrix(tra1);
-		this.scene.multMatrix(rot);
-		this.scene.multMatrix(tra2);
+        var rot = [
+            Math.cos(Math.PI / 4), Math.sin(Math.PI / 4), 0.0, 0.0,
+            -Math.sin(Math.PI / 4), Math.cos(Math.PI / 4), 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.0, 0.8, 0.0, 1.0
+        ];
 
-		this.orange.apply();
-		this.bigTriangle2.display();
+        var tra2 = [
+            1.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.0, -2.0, 0.0, 1.0
+        ];
 
-		this.scene.popMatrix();
+        this.scene.multMatrix(tra1);
+        this.scene.multMatrix(rot);
+        this.scene.multMatrix(tra2);
 
-		this.scene.pushMatrix();
+        this.bigTriangle2.texCoords = [
+            1, 0,
+            1, 1,
+            0.5, 0.5
+        ];
+        this.bigTriangle2.updateTexCoordsGLBuffers();
+        this.orange.apply();
+        this.bigTriangle2.display();
 
-		tra1 = [
-			1.0, 0.0, 0.0, 0.0,
-			0.0, 1.0, 0.0, 0.0,
-			0.0, 0.0, 1.0, 0.0,
-			0.0, -1.8, 0.0, 1.0
-		];
+        this.scene.popMatrix();
 
-		rot = [
-			Math.cos(Math.PI / 2), Math.sin(Math.PI / 2), 0.0, 0.0,
-			-Math.sin(Math.PI / 2), Math.cos(Math.PI / 2), 0.0, 0.0,
-			0.0, 0.0, 1.0, 0.0,
-			0.0, 0.8, 0.0, 1.0
-		];
+        this.scene.pushMatrix();
 
-		this.scene.multMatrix(tra1);
-		this.scene.multMatrix(rot);
+        tra1 = [
+            1.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.0, -1.8, 0.0, 1.0
+        ];
 
-		this.red.apply();
-		this.trianglesmall1.display();
+        rot = [
+            Math.cos(Math.PI / 2), Math.sin(Math.PI / 2), 0.0, 0.0,
+            -Math.sin(Math.PI / 2), Math.cos(Math.PI / 2), 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.0, 0.8, 0.0, 1.0
+        ];
 
-		this.scene.popMatrix();
+        this.scene.multMatrix(tra1);
+        this.scene.multMatrix(rot);
 
-		this.scene.pushMatrix();
+        this.trianglesmall1.texCoords = [
+            0.5, 0.5,
+            0.25, 0.75,
+            0.75, 0.75
+        ];
+        this.trianglesmall1.updateTexCoordsGLBuffers();
+        this.red.apply();
+        this.trianglesmall1.display();
 
-		tra = [
-			1.0, 0.0, 0.0, 0.0,
-			0.0, 1.0, 0.0, 0.0,
-			0.0, 0.0, 1.0, 0.0,
-			-1.0, -2.0, 0.0, 1.0
-		];
+        this.scene.popMatrix();
 
-		this.scene.multMatrix(tra);
+        this.scene.pushMatrix();
 
-		this.purple.apply();
-		this.trianglesmall2.display();
+        tra = [
+            1.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            -1.0, -2.0, 0.0, 1.0
+        ];
 
-		this.scene.popMatrix();
+        this.scene.multMatrix(tra);
 
-		this.scene.pushMatrix();
+        this.trianglesmall2.texCoords = [
+            0, 0,
+            0.25, 0.25,
+            0, 0.5
+        ];
+        this.trianglesmall2.updateTexCoordsGLBuffers();
+        this.purple.apply();
+        this.trianglesmall2.display();
 
-		rot = [
-			Math.cos(Math.PI), Math.sin(Math.PI), 0.0, 0.0,
-			-Math.sin(Math.PI), Math.cos(Math.PI), 0.0, 0.0,
-			0.0, 0.0, 1.0, 0.0,
-			0.0, 0.8, 0.0, 1.0
-		];
+        this.scene.popMatrix();
 
-		tra = [
-			1.0, 0.0, 0.0, 0.0,
-			0.0, 1.0, 0.0, 0.0,
-			0.0, 0.0, 1.0, 0.0,
-			-1.0, -3.8, 0.0, 1.0
-		];
+        this.scene.pushMatrix();
 
-		this.scene.multMatrix(tra);
-		this.scene.multMatrix(rot);
+        rot = [
+            Math.cos(Math.PI), Math.sin(Math.PI), 0.0, 0.0,
+            -Math.sin(Math.PI), Math.cos(Math.PI), 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.0, 0.8, 0.0, 1.0
+        ];
 
-		this.pink.apply();
-		this.triangle.display();
+        tra = [
+            1.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            -1.0, -3.8, 0.0, 1.0
+        ];
 
-		this.scene.popMatrix();
+        this.scene.multMatrix(tra);
+        this.scene.multMatrix(rot);
 
-		this.scene.pushMatrix();
+        this.triangle.texCoords = [
+            0, 0.5,
+            0.5, 1,
+            0, 1
+        ];
+        this.triangle.updateTexCoordsGLBuffers();
+        this.pink.apply();
+        this.triangle.display();
 
-		tra = [
-			1.0, 0.0, 0.0, 0.0,
-			0.0, 1.0, 0.0, 0.0,
-			0.0, 0.0, 1.0, 0.0,
-			-3.0, -3.0, 0.0, 1.0
-		];
+        this.scene.popMatrix();
 
-		var esc = [
-			1.0, 0.0, 0.0, 0.0,
-			0.0, -1.0, 0.0, 0.0,
-			0.0, 0.0, 1.0, 0.0,
-			0.0, 0.0, 0.0, 1.0
-		];
+        this.scene.pushMatrix();
 
-		this.scene.multMatrix(tra);
-		this.scene.multMatrix(esc);
+        tra = [
+            1.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            -3.0, -3.0, 0.0, 1.0
+        ];
 
-		this.yellow.apply();
-		this.parallelogram.display();
+        var esc = [
+            1.0, 0.0, 0.0, 0.0,
+            0.0, -1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.0, 0.0, 0.0, 1.0
+        ];
 
-		this.scene.popMatrix();
-	}
+        this.scene.multMatrix(tra);
+        this.scene.multMatrix(esc);
 
-	updateBuffers(){}
+        this.parallelogram.texCoords = [
+            0.25, 0.75,
+            0.5, 1,
+            1, 1,
+            0.5, 0.75
+        ];
+        this.parallelogram.updateTexCoordsGLBuffers();
+        this.yellow.apply();
+        this.parallelogram.display();
+
+        this.scene.popMatrix();
+    }
+
+    updateBuffers() {
+    }
 }
 
